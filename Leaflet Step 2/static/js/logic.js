b1f59ddd7a454cdbdb2efe4bd9909d78f1078717
Bella Oshomuvwe <147251051+BellaBeautiful4092023@users.noreply.github.com>
2024-02-20 18:44:57 -0600

    intial

--- Leaflet Step 2/static/js/logic.js
+++ Leaflet Step 2/static/js/logic.js
@@ -1,32 +1,31 @@
-// Define url for earthquake data/
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
 // Create myMap/
 let myMap = L.map("map", {
-    center: [40.73, -85.0023],
-    zoom: 5
+    center: [40.7, -94.5],
+    zoom: 3
 });
-let tectonicplates = new L.LayerGroup();
+
 // Add a tile layer to myMap/
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 // Retrieve and add earthquake data to the map/
-d3.json(url).then(function (data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    // Function to define style for earthquake markers
     function mapStyle(feature) {
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: mapColor(feature.geometry.coordinates[2]),
+            fillColor: getColor(feature.geometry.coordinates[2]),
             color: "black",
-            radius: mapRadius(feature.properties.mag),
+            radius: getRadius(feature.properties.mag),
             stroke: true,
             weight: 0.5
         };
     }
-    // Define colors for depth/
-    function mapColor(depth) {
+
+    // Function to define colors based on depth
+    function getColor(depth) {
         switch (true) {
             case depth > 90:
                 return "red";
@@ -42,50 +41,57 @@ d3.json(url).then(function (data) {
                 return "lightgreen";
         }
     }
-    // Calculate magnitude size/
-    function mapRadius(mag) {
+    
+    // Function to calculate marker radius based on magnitude
+    function getRadius(mag) {
         if (mag === 0) {
             return 1;
         }
         return mag * 4;
     }
 
-    L.geoJson(data, {
-
+    // Adding GeoJSON layer for earthquakes
+    let earthquakesLayer = L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng);
         },
-
         style: mapStyle,
-        // Activate pop-up data when circles are clicked/
         onEachFeature: function (feature, layer) {
             layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
         }
-    }).addTo(myMap);
-
-// Add the legend with colors to corrolate with depth
-let legend = L.control({position: "bottomright"});
-legend.onAdd = function() {
-  let div = L.DomUtil.create("div", "info legend"),
-  depth = [-10, 10, 30, 50, 70, 90];
-
-  for (let i = 0; i < depth.length; i++) {
-    div.innerHTML +=
-    '<i style="background:' + mapColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
-  }
-  return div;
-};
-legend.addTo(myMap)
-d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (platedata) {
-    // Adding our geoJSON data, along with style information, to the tectonicplates
-    // layer.
-    L.geoJson(platedata, {
-      color: "red",
-      weight: 2
     })
-      .addTo(tectonicplates);
 
-    // Then add the tectonicplates layer to the map.
-    tectonicplates.addTo(myMap);
-  });
+    // Adding legend for depth colors
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend"),
+            depth = [-10, 10, 30, 50, 70, 90];
+        for (let i = 0; i < depth.length; i++) {
+            div.innerHTML += '<i style="background:' + getColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
+
+    // Adding tectonic plates layer
+    let tectonicplates = new L.LayerGroup();
+    d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function (platedata) {
+        let tectonicPlatesLayer = L.geoJson(platedata, {
+            color: "red",
+            weight: 2
+        });
+      
+        // Create overlay object for layer control
+        let overlayMaps = {
+            "Earthquakes": earthquakesLayer,
+            "Tectonic Plates": tectonicPlatesLayer
+        
+        };// Add layer control to the map
+        L.control.layers(null, overlayMaps).addTo(myMap);
+        tectonicPlatesLayer.addTo(tectonicplates);
+        earthquakesLayer.addTo(myMap);
+
+        // Add tectonicplates LayerGroup to myMap
+        tectonicplates.addTo(myMap);
+    });
 });
