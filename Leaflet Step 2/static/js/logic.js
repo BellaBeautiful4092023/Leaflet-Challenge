073b9799f5996a10b855b85d70d4ce1f0a75dd6c
Bella Oshomuvwe <147251051+BellaBeautiful4092023@users.noreply.github.com>
2024-02-22 00:57:46 -0600

    intial

--- Leaflet Step 2/static/js/logic.js
+++ Leaflet Step 2/static/js/logic.js
@@ -5,7 +5,7 @@ let myMap = L.map("map", {
 });
 
 // Add a tile layer to myMap/
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
@@ -90,6 +90,16 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         L.control.layers(null, overlayMaps).addTo(myMap);
         tectonicPlatesLayer.addTo(tectonicplates);
         earthquakesLayer.addTo(myMap);
+
+        let baseMaps = {
+            "Grayscale": grayscale,
+            "Satellite": satellite,
+            "Outdoors": outdoors
+
+     }; // Add layer control to the map
+     L.control.layers(null, baseMaps).addTo(myMap);
+     GrayscaleLayer.addTo(grayscale);
+     earthquakesLayer.addTo(myMap);    
 
         // Add tectonicplates LayerGroup to myMap
         tectonicplates.addTo(myMap);
